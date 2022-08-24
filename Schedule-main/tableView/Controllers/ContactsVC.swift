9395a51e6a5b67e19e1c97f2907775e8fedcb2bf
VMGit <91979333+SelfishMack@users.noreply.github.com>
2022-08-24 19:06:00 +0300

    Add files via upload

---
+++ Schedule-main/tableView/Controllers/ContactsVC.swift
@@ -0,0 +1,18 @@
+//
+//  ContactsVC.swift
+//  tableView
+//
+//  Created by Владимир Макаров on 10.05.2022.
+//
+
+import UIKit
+
+class ContactsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.919511795, green: 0.8967682719, blue: 1, alpha: 1)
+        title = "Contacts"
+        // Do any additional setup after loading the view.
+    }
+}
